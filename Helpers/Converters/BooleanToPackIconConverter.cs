a44ffc7f04251e6454d586dfcd66bd3ee4658e7a
AidDeath <aiddeath@gmail.com>
2025-02-05 14:14:29 +0300

    Improve tile view

---
+++ Helpers/Converters/BooleanToPackIconConverter.cs
@@ -0,0 +1,31 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Daily_Helper.Helpers.Converters
+{
+    public class BooleanToPackIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is null) return PackIconKind.QuestionMarkCircleOutline;
+
+            if ((bool)value)
+            {
+                return PackIconKind.CheckCircleOutline;
+            }
+            return PackIconKind.CloseCircleOutline;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
